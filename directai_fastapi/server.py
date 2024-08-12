a26f3691bf469692b9431aa8bbb6054d28dc5ad9
Ben Brooks <ben@directai.io>
2024-08-12 12:56:00 -0400

    added typing

--- directai_fastapi/server.py
+++ directai_fastapi/server.py
@@ -3,17 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.post("/deploy_classifier")
-def deploy_classifier():
+def deploy_classifier() -> dict:
     return {"message": "Hello, World!"}
 
 @app.post("/deploy_detector")
-def deploy_detector():
+def deploy_detector() -> dict:
     return {"message": "Hello, World!"}
 
+# light change
 @app.get("/classify")
-def classify():
+def classify() -> dict:
     return {"message": "Hello, World!"}
 
 @app.get("/detect")
-def detect():
+def detect() -> dict:
     return {"message": "Hello, World!"}
