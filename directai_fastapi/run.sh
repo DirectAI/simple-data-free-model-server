038977f51f50e5a081d8456b8b8e27fc5ee5f3a9
Ben Brooks <ben@directai.io>
2024-08-13 14:52:27 -0400

    the bs

--- directai_fastapi/run.sh
+++ directai_fastapi/run.sh
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-#!/bin/bash
-python -m unittest unit_tests/test.py
-exec uvicorn server:app --host 0.0.0.0 --port 8000 --log-level warning
-=======
 python -m unittest unit_tests/test.py
 uvicorn server:app --host 0.0.0.0 --port 8000 --log-level warning
->>>>>>> main
