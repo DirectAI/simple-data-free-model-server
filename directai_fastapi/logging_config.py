f937b0330e5e35023a9e446949b1e568de7ff162
Ben Brooks <128745873+ben-directai@users.noreply.github.com>
2024-08-19 15:08:49 -0400

    better pre-commit (#6)
    * does this work
    
    * does this work
    
    * does this work
    
    * does this work
    
    * flake will get mad
    
    * flake will get mad
    
    * does this work
    
    * ok this should work
    
    * no more mypy ini
    
    * fixed dockerfiles
    
    * Update docker-compose.yml
    
    * changing configs

--- directai_fastapi/logging_config.py
+++ directai_fastapi/logging_config.py
@@ -1,10 +1,12 @@
 import os
 import time
 import sys
+from typing import Optional
 import logging
+
 formatter = logging.Formatter(
-    '%(asctime)s,%(msecs)03d %(levelname)-8s [%(pathname)s:%(lineno)d] %(message)s',
-    datefmt='%Y-%m-%d:%H:%M:%S'
+    "%(asctime)s,%(msecs)03d %(levelname)-8s [%(pathname)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d:%H:%M:%S",
 )
 
 
@@ -24,32 +26,38 @@ def logging_level_from_str(log_level_str: str) -> int:
         # this is the same as INFO (default)
         return 20
 
+
 def grab_importing_fp() -> str:
-    return sys.modules['__main__'].__file__
+    to_return = sys.modules["__main__"].__file__
+    if to_return is None:
+        to_return = ""
+    return to_return
+
 
 # Find the most recently created directory in 'logs'
-logs_dir = 'logs'
-subdirs = [os.path.join(logs_dir, d) for d in os.listdir(logs_dir) if os.path.isdir(os.path.join(logs_dir, d))]
+logs_dir = "logs"
+subdirs = [
+    os.path.join(logs_dir, d)
+    for d in os.listdir(logs_dir)
+    if os.path.isdir(os.path.join(logs_dir, d))
+]
 latest_subdir = max(subdirs, key=os.path.getctime)
 
 # find the file t hat imported this to build a descriptive log filename
 main_file = grab_importing_fp()
-if main_file is None:
-    log_filename = ""
-elif isinstance(main_file, str):
-    main_file = main_file.replace('/', '_')
+main_file = main_file.replace("/", "_")
 log_filename = os.path.join(latest_subdir, f"{main_file}_runtime.log")
 assert isinstance(formatter._fmt, str)
 logging.basicConfig(
     filename=log_filename,
     format=formatter._fmt,
     datefmt=formatter.datefmt,
-    filemode='a',
-    level=logging.INFO
+    filemode="a",
+    level=logging.INFO,
 )
-    
+
 logger = logging.getLogger(__name__)
-log_level_str = os.getenv('LOGGING_LEVEL', 'INFO').upper()
+log_level_str = os.getenv("LOGGING_LEVEL", "INFO").upper()
 console_handler = logging.StreamHandler()
 console_log_level = logging_level_from_str(log_level_str)
 console_handler.setLevel(console_log_level)
