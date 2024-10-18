4781586337cb08c5fde337be7c22d42506e721bb
Isaac Robinson <isaac@directai.io>
2024-10-18 19:43:20 +0000

    updating docs pre release

--- directai_fastapi/run.sh
+++ directai_fastapi/run.sh
@@ -1,5 +1,7 @@
 timestamp=$(date +%s)
 log_dir="logs/fastapi_$timestamp"
 mkdir -p "$log_dir"
+echo "Running unit tests..."
 python -m unittest unit_tests/test.py
+echo "Done running unit tests, launching server..."
 uvicorn server:app --host 0.0.0.0 --port 8000 --log-level warning
